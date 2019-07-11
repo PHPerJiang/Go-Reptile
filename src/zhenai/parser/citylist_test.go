67f42a39ead7e5b9d33c3042f9446048f5e323f2
PHPerJiang <554246432@qq.com>
2019-07-11 11:18:34 +0800

    城市解析器测试

--- src/zhenai/parser/citylist_test.go
+++ src/zhenai/parser/citylist_test.go
@@ -1,1 +1,21 @@
 package parser
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestParseCityList(t *testing.T) {
+	content, err := ioutil.ReadFile("test.html")
+	if err != nil {
+		panic(err)
+	}
+	result := ParseCityList(content)
+	const resultSize  = 470
+	if len(result.Requests) != resultSize {
+		t.Errorf("Result should have %d requests, but had %d requests now",resultSize, len(result.Requests))
+	}
+	if len(result.Items) != resultSize {
+		t.Errorf("Result should have %d items, but had %d items now", resultSize, len(result.Items))
+	}
+}
